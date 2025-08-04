82f9e22cf3ee970bf30d1344cc3be4589e5a6272
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2025-08-04 12:57:55 -0700

    canonicalLink

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -3,10 +3,14 @@ import { SeoProps } from "../utils/types";
 
 export default function Seo(props: SeoProps) {
   const baseUrl =
-    // const baseUrl =
     process.env.NEXT_PUBLIC_BASE_URL || "https://www.evnhandyman.com";
   const canonicalUrl =
     process.env.NEXT_PUBLIC_CANONICAL_URL || "https://www.evnhandyman.com";
+
+  const canonicalLink =
+    !props.page || props.page === "home"
+      ? canonicalUrl
+      : `${canonicalUrl}/${props.page}`;
 
   return (
     <Head>
@@ -18,12 +22,8 @@ export default function Seo(props: SeoProps) {
         property="og:image"
         content={`${baseUrl}/handyman_evn_logo_light.png`}
       />
-      <meta property="og:url" content={`${canonicalUrl}/${props.page}`} />
-      <link
-        rel="canonical"
-        href={`${canonicalUrl}/${props.page}`}
-        key="canonical"
-      />
+      <meta property="og:url" content={canonicalLink} />
+      <link rel="canonical" href={canonicalLink} key="canonical" />
     </Head>
   );
 }
